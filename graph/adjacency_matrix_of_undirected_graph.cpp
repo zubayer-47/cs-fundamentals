476ea18020e74e5dca4103eb6cda9ba707571201
A B M Zubayer <77219792+zubayer-47@users.noreply.github.com>
2025-08-17 01:03:26 +0000

    learn: start with graph data structure and learnt adjacency matrix's directed and undirected graph

---
+++ graph/adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 5 6
+// 0 1
+// 0 3
+// 0 2
+// 0 4
+// 1 4
+// 2 4
+
+// 1 1 1 1 1 
+// 1 1 0 0 1 
+// 1 0 1 0 1 
+// 1 0 0 1 0 
+// 1 1 1 0 1 
+
+int main()
+{
+    int n, c;
+    cin >> n >> c;
+
+    int adjacency_matrix[n][n];
+
+    memset(adjacency_matrix, 0, sizeof(adjacency_matrix));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adjacency_matrix[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < c; i++)
+    {
+        int r, l;
+        cin >> r >> l;
+
+        adjacency_matrix[r][l] = 1;
+        adjacency_matrix[l][r] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << adjacency_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
